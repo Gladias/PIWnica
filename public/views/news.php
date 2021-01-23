1fbb9e4726db0f7ddc79c36a15518f34b4bbaa1f
Gladias <alek.drozdz@gmail.com>
2021-01-23 02:46:51 +0100

    Add not finished views for news and starting page

---
+++ public/views/news.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="public/css/nav-bar.css">
+    <link rel="stylesheet" type="text/css" href="public/css/news-style.css">
+    <script src="https://kit.fontawesome.com/6632ce43ac.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@600&display=swap" rel="stylesheet">
+    <title>Piwne newsy</title>
+</head>
+<body>
+    <div class="container">
+        <?php include('nav-bar.php') ?>
+        <div class="main-page">
+            <div class="news-title">
+                <h2>Piwne newsy</h2>
+            </div>
+            <div class="post-area">
+                <div class="popular">
+                    <div class="post">
+                        <img src="public/img/news-placeholder.jpg" alt="Post image">
+                        <div class="title">
+                            Excepteur sint occaecat cupidatat non.
+                        </div>
+                        <div class="description">
+                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit ani.
+                        </div>
+                        <div class="author">
+                            <i class="fas fa-user-edit"></i>
+                            1h ago
+                        </div>
+                        <div class="time">
+                            <i class="far fa-clock"></i>
+                            1h ago
+                        </div>
+                    </div>
+                </div>
+                <div class="new">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ public/views/start.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="public/css/nav-bar.css">
+    <link rel="stylesheet" type="text/css" href="public/css/start.css">
+    <script src="https://kit.fontawesome.com/6632ce43ac.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@600&display=swap" rel="stylesheet">
+    <title>Strona główna</title>
+</head>
+<body>
+<div class="container">
+    <?php include('nav-bar.php') ?>
+    <div class="main-page">
+    </div>
+</div>
+</body>
+</html>
